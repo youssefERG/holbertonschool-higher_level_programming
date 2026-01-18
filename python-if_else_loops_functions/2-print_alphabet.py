9d629056253400c1948ac17bafef5eff6d02171b
youssefERG <erengamer555@gmail.com>
2026-01-18 21:03:56 +0100

    fixes

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-"""Print the lowercase English alphabet without a newline.
-
-Demonstrates a simple for loop, ASCII codes, and chr() to convert integer
-code points to characters. Each character is printed without a newline by
-using end=''.
-"""
 for i in range(97, 123):
-    print("{}".format(chr(i)), end='')
+    print("{:c}".format(i), end="")
