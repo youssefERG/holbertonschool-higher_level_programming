1e5174ba789a8022ec60bc3ff8d14514fda05697
youssefERG <erengamer555@gmail.com>
2026-01-30 12:57:03 +0100

    1 square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a Square with a given size."""
+        self.__size = size
