fcac47e78aa125083555d60788c1f8192c622dca
youssefERG <erengamer555@gmail.com>
2026-02-08 21:06:02 +0100

    finaly this project is done after 4 days

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines a function to return the list of available attributes and methods of an object."""
+"""Module that defines a function to return the list of available attributes
+and methods of an object.
+"""
 
 
 def lookup(obj):
