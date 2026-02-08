449402fb6f53ffff0d7ae5e5aa6539304bd0851b
youssefERG <erengamer555@gmail.com>
2026-02-08 21:17:28 +0100

    pip validation

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,7 +5,7 @@ object is an instance of a class or its subclass.
 
 
 def is_kind_of_class(obj, a_class):
-	"""Return True if obj is an instance
-	of a_class or its subclass, otherwise False.
-	"""
-	return isinstance(obj, a_class)
+    """Return True if obj is an instance
+    of a_class or its subclass, otherwise False.
+    """
+    return isinstance(obj, a_class)
