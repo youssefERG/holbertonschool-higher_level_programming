58590d4def6dc7587d830a9240cc4959526d7503
youssefERG <erengamer555@gmail.com>
2026-02-08 21:14:46 +0100

    pip validation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,5 +5,7 @@ is exactly an instance of a specified class.
 
 
 def is_same_class(obj, a_class):
-    """Return True if obj is exactly an instance of a_class, otherwise False."""
+    """Return True if obj is exactly an instance of a_class,
+    otherwise False.
+    """
     return type(obj) is a_class
