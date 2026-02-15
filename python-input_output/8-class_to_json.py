ebdcfde8480bb53048b044b8b92de1280bd6b194
youssefERG <erengamer555@gmail.com>
2026-02-15 12:10:21 +0100

    python-input_output

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""Module that returns the dictionary
-description for JSON serialization.
-"""
+"""Module that returns the dictionary description of an object for
+JSON serialization."""
 
 
 def class_to_json(obj):
-    """Return the dictionary description
-	 of an object for JSON serialization.
-	 """
+    """Return the dictionary description of an object for JSON serialization."""
     return obj.__dict__
